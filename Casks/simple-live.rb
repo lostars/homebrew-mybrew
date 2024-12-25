21101f79947b78d1feb85f61e07822abceb08d03
lostars <746320542jn@gmail.com>
2024-12-26 01:16:42 +0800

    add simple-live cask

---
+++ Casks/simple-live.rb
@@ -0,0 +1,9 @@
+cask "simple-live" do
+  version "1.7.5"
+  url "https://github.com/xiaoyaocz/dart_simple_live/releases/download/v1.7.5/simple_live_app-1.7.5+10705-macos.dmg"
+  name "simple-live"
+  desc "简简单单的看直播"
+  homepage "https://github.com/xiaoyaocz/dart_simple_live"
+
+  app "Simple Live.app"
+end
