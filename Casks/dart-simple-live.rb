abd33d79cb40e46d515de8ae990e67840c9d6223
lostars <746320542jn@gmail.com>
2024-12-30 15:54:05 +0800

    update simple-live

--- Casks/dart-simple-live.rb
+++ Casks/dart-simple-live.rb
@@ -1,7 +1,6 @@
 cask "dart-simple-live" do
   version "1.7.5"
-  revision "10705"
-  url "https://github.com/xiaoyaocz/dart_simple_live/releases/download/v#{version}/simple_live_app-#{version}+#{revision}-macos.dmg"
+  url "https://github.com/xiaoyaocz/dart_simple_live/releases/download/v#{version}/simple_live_app-#{version}+10705-macos.dmg"
   name "simple_live"
   desc "简简单单的看直播"
   homepage "https://github.com/xiaoyaocz/dart_simple_live"
