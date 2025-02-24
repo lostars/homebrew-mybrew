f311a516c3e1647d07305cc3b1a5c5a3b0b67153
lostars <746320542jn@gmail.com>
2025-02-24 23:09:58 +0800

    add GitHub Actions

---
+++ update.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+# update "user" "repo" "filename" "filter"
+function update() {
+  user=$1
+  repo=$2
+  filename=$3
+  filter=$4
+
+  url="https://api.github.com/repos/${user}/${repo}/releases"
+  output="output/${user}-${repo}.json"
+  curl -o "${output}" "$url"
+
+  for asset in $(jq -c 'map(select(.prerelease == false)) | .[0] | .assets[]' "${output}"); do
+    name=$(echo "$asset" | jq -r '.name')
+    if [[ $name =~ $filter ]]; then
+      version="${BASH_REMATCH[1]}"
+
+      v=$(sed -n 's/.*version "\([^"]*\)".*/\1/p' $filename)
+      if [ "$v" == "$version" ]; then
+        echo "$filename no updates current version: $version"
+      else
+        echo "$filename update version: from xxx => $version"
+      fi
+    fi
+  done
+}
+
+if [ ! -d "output" ]; then
+    mkdir "output"
+fi
+
+for config in $(jq -c '.config[]' "config.json"); do
+  name=$(echo "$config" | jq -r '.name')
+  version=$(echo "$config" | jq -r '.version')
+
+  repoUrl=$(sed -n 's/.*homepage "\([^"]*\)".*/\1/p' "$name")
+  user=$(echo "$repoUrl" | sed -n 's|https://github.com/\([^/]*\)/\([^/]*\)|\1|p')
+  repo=$(echo "$repoUrl" | sed -n 's|https://github.com/\([^/]*\)/\([^/]*\)|\2|p')
+
+  update "$user" "$repo" "$name" "$version"
+done
