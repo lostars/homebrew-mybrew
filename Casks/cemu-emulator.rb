f9e81674f025fca0a637f428ecccccc960d39a18
lostars <746320542jn@gmail.com>
2025-02-24 16:44:11 +0000

    cron updates

--- Casks/cemu-emulator.rb
+++ Casks/cemu-emulator.rb
@@ -1,5 +1,5 @@
 cask "cemu-emulator" do
-  version "2.4"
+  version "2.5"
   url "https://github.com/cemu-project/Cemu/releases/download/v#{version}/cemu-#{version}-macos-12-x64.dmg"
   name "cemu-emulator"
   desc "Cemu - Wii U emulator"
