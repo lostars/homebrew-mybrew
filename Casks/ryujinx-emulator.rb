0f32172f265066cea0516da38f1a0d7fdc123a64
lostars <746320542jn@gmail.com>
2025-03-17 16:10:56 +0000

    update Stable-Releases from 1.2.82 to 1.2.86

--- Casks/ryujinx-emulator.rb
+++ Casks/ryujinx-emulator.rb
@@ -1,5 +1,5 @@
 cask "ryujinx-emulator" do
-  version "1.2.82"
+  version "1.2.86"
   url "https://github.com/Ryubing/Stable-Releases/releases/download/#{version}/ryujinx-#{version}-macos_universal.app.tar.gz"
   name "Ryujinx"
   desc "Ryujinx is an open-source Nintendo Switch emulator, originally created by gdkchan, written in C#"
