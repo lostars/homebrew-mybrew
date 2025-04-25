76c56d2598ea3cd40c1b31ba80ca058de6767fa1
lostars <746320542jn@gmail.com>
2025-04-25 15:50:51 +0800

    add ryujinx canary

---
+++ Casks/ryujinx-emulator@canary.rb
@@ -0,0 +1,13 @@
+cask "ryujinx-emulator@canary" do
+  version "1.3.36"
+  url "https://github.com/Ryubing/Canary-Releases/releases/download/#{version}/ryujinx-#{version}-macos_universal.app.tar.gz"
+  name "Ryujinx"
+  desc "Ryujinx is an open-source Nintendo Switch emulator, originally created by gdkchan, written in C#"
+  homepage "https://github.com/Ryubing/Canary-Releases"
+
+  app "Ryujinx(Canary).app"
+
+  zap trash: [
+    "~/Library/Application Support/Ryujinx"
+  ]
+end
