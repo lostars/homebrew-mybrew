54a197b1b4a70a899d76bce22bf426dbd877bd45
lostars <746320542jn@gmail.com>
2025-04-25 16:42:45 +0800

    update NX Optimizer desc

--- Casks/nx-optimizer.rb
+++ Casks/nx-optimizer.rb
@@ -3,7 +3,7 @@ cask "nx-optimizer" do
   arc = Hardware::CPU.intel? ? "Intel" : "Silicon"
   url "https://github.com/MaxLastBreath/nx-optimizer/releases/download/manager-#{version}/NX_Optimizer_#{version}_MacOS_#{arc}.zip"
   name "nx-optimizer"
-  desc "NX-Optimizer (Known As TOTK-Optimizer) is a Mod Manager for UltraCam, a code injection mod for the following Games: \rThe Legend of Zelda: Tears of the Kingdom, Breath Of The Wild, Echoes Of Wisdom, Link's Awakening and More"
+  desc "NX-Optimizer (Known As TOTK-Optimizer) is a Mod Manager for UltraCam, a code injection mod for the following Games: The Legend of Zelda: Tears of the Kingdom, Breath Of The Wild, Echoes Of Wisdom, Link's Awakening and More"
   homepage "https://github.com/MaxLastBreath/nx-optimizer"
 
   app "NX Optimizer.app"
