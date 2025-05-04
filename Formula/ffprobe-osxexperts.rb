78bc313126eb92e58c5f4070de2844f7ba956f23
lostars <746320542jn@gmail.com>
2025-05-04 12:31:54 +0800

    fix version

--- Formula/ffprobe-osxexperts.rb
+++ Formula/ffprobe-osxexperts.rb
@@ -1,19 +1,18 @@
 class FfprobeOsxexperts < Formula
   desc "FFmpeg static builds from OSXExperts"
   homepage "https://www.osxexperts.net/"
-  version ""
-
-  ARM_VERSION = "7.1.1"
-  INTEL_VERSION = "7.1"
 
   if Hardware::CPU.arm?
-    version = ARM_VERSION.tr('.', '')
-    url "https://www.osxexperts.net/ffmpeg#{version}arm.zip"
+    url "https://www.osxexperts.net/ffprobe711arm.zip"
     sha256 "ae77d6751f4db81098a11dcc966a8d098925411430169475c8f8a7bfad76188b"
   else
-    version = INTEL_VERSION.tr('.', '')
-    url "https://www.osxexperts.net/ffmpeg#{version}intel.zip"
+    url "https://www.osxexperts.net/ffprobe71intel.zip"
     sha256 "ce5414269f0efa1e88b5e23b57f801d5b9a40be554716544936e0332b4601a62"
+  end
+
+  version do
+      match = url.match(/ffmpeg(\d)(\d)(\d)(arm|intel)\.zip/)
+      next "#{match[1]}.#{match[2]}.#{match[3]}" if match
   end
 
   def install
