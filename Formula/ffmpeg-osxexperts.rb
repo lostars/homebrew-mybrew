59bebd0f39b6c78379f386493ed91b45e3a9ab4d
lostars <746320542jn@gmail.com>
2025-05-04 13:02:55 +0800

    fix version

--- Formula/ffmpeg-osxexperts.rb
+++ Formula/ffmpeg-osxexperts.rb
@@ -5,19 +5,9 @@ class FfmpegOsxexperts < Formula
   if Hardware::CPU.arm?
     version "7.1.1"
     url "https://www.osxexperts.net/ffmpeg#{version.to_s.gsub('.', '')}arm.zip"
-    sha256 do
-      require "open-uri"
-      require "digest"
-      Digest::SHA256.hexdigest(URI.open(url).read)
-    end
   else
     version "7.1"
     url "https://www.osxexperts.net/ffmpeg#{version.to_s.gsub('.', '')}intel.zip"
-    sha256 do
-      require "open-uri"
-      require "digest"
-      Digest::SHA256.hexdigest(URI.open(url).read)
-    end
   end
 
   def install
--- Formula/ffprobe-osxexperts.rb
+++ Formula/ffprobe-osxexperts.rb
@@ -5,11 +5,9 @@ class FfprobeOsxexperts < Formula
   if Hardware::CPU.arm?
     version "7.1.1"
     url "https://www.osxexperts.net/ffprobe#{version.to_s.gsub('.', '')}arm.zip"
-    sha256 "ae77d6751f4db81098a11dcc966a8d098925411430169475c8f8a7bfad76188b"
   else
     version "7.1"
     url "https://www.osxexperts.net/ffprobe#{version.to_s.gsub('.', '')}intel.zip"
-    sha256 "ce5414269f0efa1e88b5e23b57f801d5b9a40be554716544936e0332b4601a62"
   end
 
   def install
