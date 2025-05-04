409f43e29bf354d6cf37e66aa6db30c1b23c0226
lostars <746320542jn@gmail.com>
2025-05-04 12:52:42 +0800

    fix version

--- Formula/ffmpeg-osxexperts.rb
+++ Formula/ffmpeg-osxexperts.rb
@@ -1,18 +1,14 @@
 class FfmpegOsxexperts < Formula
   desc "FFmpeg static builds from OSXExperts"
   homepage "https://www.osxexperts.net/"
-  version ""
-
-  ARM_VERSION = "7.1.1"
-  INTEL_VERSION = "7.1"
 
   if Hardware::CPU.arm?
-    version = ARM_VERSION.tr('.', '')
-    url "https://www.osxexperts.net/ffmpeg#{version}arm.zip"
+    version "7.1.1"
+    url "https://www.osxexperts.net/ffmpeg#{version.to_s.gsub('.', '')}arm.zip"
     sha256 "011221d75eae36943b5a6a28f70e25928cfb5602fe616d06da0a3b9b55ff6b75"
   else
-    version = INTEL_VERSION.tr('.', '')
-    url "https://www.osxexperts.net/ffmpeg#{version}intel.zip"
+    version "7.1"
+    url "https://www.osxexperts.net/ffmpeg#{version.to_s.gsub('.', '')}intel.zip"
     sha256 "4a4a968b98859588e98500ae25973d80a5ca5eed0724222b9f76360dcb72a001"
   end
 
--- Formula/ffprobe-osxexperts.rb
+++ Formula/ffprobe-osxexperts.rb
@@ -3,16 +3,13 @@ class FfprobeOsxexperts < Formula
   homepage "https://www.osxexperts.net/"
 
   if Hardware::CPU.arm?
-    url "https://www.osxexperts.net/ffprobe711arm.zip"
+    version "7.1.1"
+    url "https://www.osxexperts.net/ffprobe#{version.to_s.gsub('.', '')}arm.zip"
     sha256 "ae77d6751f4db81098a11dcc966a8d098925411430169475c8f8a7bfad76188b"
   else
-    url "https://www.osxexperts.net/ffprobe71intel.zip"
+    version "7.1"
+    url "https://www.osxexperts.net/ffprobe#{version.to_s.gsub('.', '')}intel.zip"
     sha256 "ce5414269f0efa1e88b5e23b57f801d5b9a40be554716544936e0332b4601a62"
-  end
-
-  version do
-      match = url.match(/ffmpeg(\d)(\d)(\d)(arm|intel)\.zip/)
-      next "#{match[1]}.#{match[2]}.#{match[3]}" if match
   end
 
   def install
