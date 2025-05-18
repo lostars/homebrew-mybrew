8e182aa6d5fec80a4adbd6e1bbe66c3050744c72
lostars <746320542jn@gmail.com>
2025-05-18 16:08:55 +0000

    update qbit-cli from 0.4.0 to 0.5.0 update Canary-Releases from 1.3.44 to 1.3.45

--- Formula/qbit-cli.rb
+++ Formula/qbit-cli.rb
@@ -1,7 +1,7 @@
 class QbitCli < Formula
   desc "Another qBittorrent CLI"
   homepage "https://github.com/lostars/qbit-cli"
-  version "0.4.0"
+  version "0.5.0"
   url "https://github.com/lostars/qbit-cli/releases/download/v#{version}/qbit_#{version}_darwin_arm64.tar.gz"
 
   depends_on arch: :arm64
