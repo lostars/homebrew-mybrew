127481ccf14413a9aa2c4c71d3c9d29e2775c94e
lostars <746320542jn@gmail.com>
2025-06-04 11:03:01 +0800

    update ryujinx latest repo

--- Casks/ryujinx-emulator.rb
+++ Casks/ryujinx-emulator.rb
@@ -1,9 +1,9 @@
 cask "ryujinx-emulator" do
   version "1.3.1"
-  url "https://github.com/Ryubing/Stable-Releases/releases/download/#{version}/ryujinx-#{version}-macos_universal.app.tar.gz"
+  url "https://github.com/iurehg8uetgyh8ui5e/sr/releases/download/#{version}/ryujinx-#{version}-macos_universal.app.tar.gz"
   name "Ryujinx"
   desc "Ryujinx is an open-source Nintendo Switch emulator, originally created by gdkchan, written in C#"
-  homepage "https://github.com/Ryubing/Stable-Releases"
+  homepage "https://github.com/iurehg8uetgyh8ui5e/sr"
 
   app "Ryujinx.app"
 
--- Casks/ryujinx-emulator@canary.rb
+++ Casks/ryujinx-emulator@canary.rb
@@ -1,9 +1,9 @@
 cask "ryujinx-emulator@canary" do
-  version "1.3.58"
-  url "https://github.com/Ryubing/Canary-Releases/releases/download/#{version}/ryujinx-#{version}-macos_universal.app.tar.gz"
+  version "1.3.69"
+  url "https://github.com/iurehg8uetgyh8ui5e/cr/releases/download/#{version}/ryujinx-#{version}-macos_universal.app.tar.gz"
   name "Ryujinx"
   desc "Ryujinx is an open-source Nintendo Switch emulator, originally created by gdkchan, written in C#"
-  homepage "https://github.com/Ryubing/Canary-Releases"
+  homepage "https://github.com/iurehg8uetgyh8ui5e/cr"
 
   app "Ryujinx(Canary).app"
 
