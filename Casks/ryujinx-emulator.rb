26b88507d10c8c91afd55855c3b75e73099215f6
lostars <746320542jn@gmail.com>
2025-06-30 15:06:47 +0800

    auto update ryujinx

--- Casks/ryujinx-emulator.rb
+++ Casks/ryujinx-emulator.rb
@@ -1,9 +1,9 @@
 cask "ryujinx-emulator" do
   version "1.3.2"
-  url "https://github.com/iurehg8uetgyh8ui5e/sr/releases/download/#{version}/ryujinx-#{version}-macos_universal.app.tar.gz"
+  url "https://git.ryujinx.app/api/v4/projects/1/packages/generic/Ryubing/#{version}/ryujinx-#{version}-macos_universal.app.tar.gz"
   name "Ryujinx"
   desc "Ryujinx is an open-source Nintendo Switch emulator, originally created by gdkchan, written in C#"
-  homepage "https://github.com/iurehg8uetgyh8ui5e/sr"
+  homepage "https://git.ryujinx.app/ryubing/ryujinx"
 
   app "Ryujinx.app"
 
--- update.sh
+++ update.sh
@@ -33,6 +33,30 @@ function update() {
   done
 }
 
+function updateGitlab() {
+  domain=$1
+  user=$2
+  repo=$3
+  filename=$4
+
+  url="https://$domain/api/v4/projects/$user%2F$repo/releases"
+  version=$(curl -s "$url" | jq -r '[.[] | select(.tag_name | test("beta|alpha|rc"; "i") | not)][0].tag_name')
+
+  v=$(sed -n 's/.*version "\([^"]*\)".*/\1/p' "$filename")
+  if [ "$v" == "$version" ]; then
+    echo "$filename no updates current version: $version"
+  else
+    if [[ "$os" == "Darwin" ]]; then
+      sed -i '' "s/version \"$v\"/version \"$version\"/" "$filename"
+    else
+      sed -i "s/version \"$v\"/version \"$version\"/" "$filename"
+    fi
+    echo "true" > output/status.txt
+    echo "$filename update $repo from $v to $version" >> output/message.txt
+    echo "$filename update $repo from $v to $version"
+  fi
+}
+
 if [ ! -d "output" ]; then
     mkdir "output"
 fi
@@ -43,6 +67,7 @@ for config in $(jq -c '.config[]' "config.json"); do
   name=$(echo "$config" | jq -r '.name')
   version=$(echo "$config" | jq -r '.version')
   autoUpdate=$(echo "$config" | jq -r '.autoUpdate')
+  type=$(echo "$config" | jq -r '.type')
   if [ "$autoUpdate" == "false" ]; then
       echo "$name disable auto update"
       continue
@@ -53,5 +78,10 @@ for config in $(jq -c '.config[]' "config.json"); do
     user=$(echo "$repoUrl" | sed -n 's|https://github.com/\([^/]*\)/\([^/]*\)|\1|p')
     repo=$(echo "$repoUrl" | sed -n 's|https://github.com/\([^/]*\)/\([^/]*\)|\2|p')
     update "$user" "$repo" "$name" "$version"
+  elif [[ "$type" == "gitlab" ]]; then
+    user=$(echo "$repoUrl" | sed -n 's|https://[^/]*/\([^/]*\)/\([^/]*\)|\1|p')
+    repo=$(echo "$repoUrl" | sed -n 's|https://[^/]*/\([^/]*\)/\([^/]*\)|\2|p')
+    domain=$(echo "$repoUrl" | sed -n 's|https://\([^/]*\)/\([^/]*\)/\([^/]*\)|\1|p')
+    updateGitlab "$domain" "$user" "$repo" "$name"
   fi
 done
