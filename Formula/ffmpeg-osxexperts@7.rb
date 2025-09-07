ca5473978b453c5121c924d04844d9f9f523c939
lostars <746320542jn@gmail.com>
2025-09-07 22:24:47 +0800

    update ffmpeg to 8.0 and keep 7.x version

--- Formula/ffmpeg-osxexperts@7.rb
+++ Formula/ffmpeg-osxexperts@7.rb
@@ -3,10 +3,10 @@ class FfmpegOsxexperts < Formula
   homepage "https://www.osxexperts.net/"
 
   if Hardware::CPU.arm?
-    version "8.0"
+    version "7.1.1"
     url "https://www.osxexperts.net/ffmpeg#{version.to_s.gsub('.', '')}arm.zip"
   else
-    version "8.0"
+    version "7.1"
     url "https://www.osxexperts.net/ffmpeg#{version.to_s.gsub('.', '')}intel.zip"
   end
 
--- Formula/ffprobe-osxexperts@7.rb
+++ Formula/ffprobe-osxexperts@7.rb
@@ -1,12 +1,12 @@
-class FfprobeOsxexperts < Formula
+class FfprobeOsxexperts@7 < Formula
   desc "FFmpeg static builds from OSXExperts"
   homepage "https://www.osxexperts.net/"
 
   if Hardware::CPU.arm?
-    version "8.0"
+    version "7.1.1"
     url "https://www.osxexperts.net/ffprobe#{version.to_s.gsub('.', '')}arm.zip"
   else
-    version "8.0"
+    version "7.1"
     url "https://www.osxexperts.net/ffprobe#{version.to_s.gsub('.', '')}intel.zip"
   end
 
