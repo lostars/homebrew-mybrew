a1e9abbec6e0b6c8f630ed81446d86b54e578138
lostars <746320542jn@gmail.com>
2025-11-29 17:03:29 +0800

    update simple_live version

--- Casks/dart-simple-live.rb
+++ Casks/dart-simple-live.rb
@@ -1,6 +1,6 @@
 cask "dart-simple-live" do
   version "1.11.2"
-  version_with_zeros = version.tr('.', '0')
+  version_with_zeros = version.split(".").map { |v| v.to_i.to_s.rjust(2, "0") }.join
   url "https://github.com/lostars/dart_simple_live/releases/download/v#{version}/simple_live_app-#{version}+#{version_with_zeros}-macos.dmg"
   name "simple_live"
   desc "简简单单的看直播"
