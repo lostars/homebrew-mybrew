f16864f99412a7ead02c52b366de6a004ae10b4e
lostars <746320542jn@gmail.com>
2026-04-22 22:24:00 +0800

    fix neovim install

--- Formula/neovim.rb
+++ Formula/neovim.rb
@@ -2,14 +2,18 @@ class Neovim < Formula
   desc "Ambitious Vim-fork focused on extensibility and agility"
   homepage "https://github.com/neovim/neovim"
   license "Apache-2.0"
-  version "0.12.0"
+  version "0.12.1"
   url "https://github.com/neovim/neovim/releases/download/v#{version}/nvim-macos-arm64.tar.gz"
 
   depends_on arch: :arm64
   depends_on :macos
 
   def install
-    bin.install "nvim-macos-arm64/bin/nvim"
+    cd Dir["nvim-macos-arm64"].first do
+      bin.install Dir["bin/*"]
+      lib.install Dir["lib/*"]   if Dir.exist?("lib")
+      share.install Dir["share/*"] if Dir.exist?("share")
+    end
   end
 
 end
